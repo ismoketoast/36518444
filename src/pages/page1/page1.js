3f15c6b9f990ee73b110eef655969b4dc42bf34e
ismoketoast <ismoketoast@gmail.com>
2020-12-29 16:49:42 -0500

    Fix styling

--- src/pages/page1/page1.js
+++ src/pages/page1/page1.js
@@ -6,25 +6,25 @@ import { Parallax } from 'react-scroll-parallax';
 export const Page1 = () => {
   return (
     <div className="App">
-    <div className="page-1">
-      <Parallax y={[-200, 200]} className="image-1">
-        <img src={logo} className="App-logo" alt="logo" />
-      </Parallax>
-      <Parallax y={[-20, 20]} className="image-2">
-        <img src={logo} className="App-logo" alt="logo" />
-      </Parallax>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <div className="page-1">
+        <Parallax y={[-200, 200]} className="image-1">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Parallax>
+        <Parallax y={[-20, 20]} className="image-2">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Parallax>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
     </div>
-  </div>
   );
 }
